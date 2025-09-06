6e5c7b78d4b9e5546650085fa3ffa620ade4b744
SATEESH-ABBIGERI <sateeshabbigeri4@gmail.com>
2025-09-06 13:40:38 +0530

    rtl desing

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module Manhat_tapeout (
+module tt_um_Manhat_tapeout (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
